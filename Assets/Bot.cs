ab79b912997df3358c0997dc42ced255fe635421
kashi <41294552+kashibal@users.noreply.github.com>
2024-07-16 00:30:50 +0900

    all projects data

---
+++ Assets/Bot.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    float speed = 50;
+    Animator animator;
+    public Transform ball;
+    public Transform aimTarget;
+    float force = 13;
+
+    public Transform[] targets;
+
+    Vector3 targetPosition;
+
+    ShotManager shotManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+        animator = GetComponent<Animator>();
+        shotManager = GetComponent<ShotManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        targetPosition.x = ball.position.x;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+    Vector3 PickTarget()
+    {
+        int randomValue = Random.Range(0, targets.Length);
+        return targets[randomValue].position;
+    }
+    Shot PickShot()
+    {
+        int randomValue = Random.Range(0, 2);
+        if(randomValue == 0)
+        {
+            return shotManager.topSpin;
+        }
+        else
+        {
+            return shotManager.flat;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Shot currentShot = PickShot();
+
+            Vector3 dir = PickTarget() - transform.position;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * currentShot.hitForce + new Vector3(0, currentShot.upForce, 0);
+
+            Vector3 ballDir = ball.position - transform.position;
+            if (ballDir.x >= 0)
+            {
+                animator.Play("forehand");
+            }
+            else
+            {
+                animator.Play("backhand");
+            }
+        }
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Transform aimTarget;
+    float speed = 3f;
+    float force = 10;
+
+    bool hitting;
+
+    public Transform ball;
+    Animator animator;
+
+    ShotManager shotManager;
+    Shot currentShot;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        shotManager = GetComponent<ShotManager>();
+        currentShot = shotManager.topSpin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            hitting = true;
+            currentShot = shotManager.topSpin;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.F))
+        {
+            hitting = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            hitting = true;
+            currentShot = shotManager.flat;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            hitting = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            hitting = true;
+            currentShot = shotManager.flatServe;
+            GetComponent<BoxCollider>().enabled = false;
+            animator.Play("serve-prepare");
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            hitting = true;
+            currentShot = shotManager.kickServe;
+            GetComponent<BoxCollider>().enabled = false;
+            animator.Play("serve-prepare");
+        }
+
+        if (Input.GetKeyUp(KeyCode.R) || Input.GetKeyUp(KeyCode.T))
+        {
+            hitting = false;
+            GetComponent<BoxCollider>().enabled = true;
+            ball.transform.position = transform.position + new Vector3(0.2f, 2, 0);
+            Vector3 dir = aimTarget.position - transform.position;
+            ball.GetComponent<Rigidbody>().velocity = dir.normalized * currentShot.hitForce + new Vector3(0, currentShot.upForce, 0);
+            animator.Play("serve");
+        }
+
+        if (hitting)
+        {
+            aimTarget.Translate( new Vector3(h, 0, 0) * speed * 2 * Time.deltaTime);
+        }
+
+        if ( (h != 0 || v != 0) && !hitting)
+        {
+            transform.Translate( new Vector3(h, 0, v) * speed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Vector3 dir = aimTarget.position - transform.position;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * currentShot.hitForce + new Vector3(0, currentShot.upForce, 0);
+
+            Vector3 ballDir = ball.position - transform.position;
+            if (ballDir.x >= 0)
+            {
+                animator.Play("forehand");
+            }
+            else
+            {
+                animator.Play("backhand");
+            }
+        }
+    }
+}
